0bbcd4125900d0cb079c045cd6f4ee04bcb2d51a
ohadz <ohadzisserman@gmail.com>
2023-06-01 15:43:28 +0300

    25.05.23

--- resunance_animation.py
+++ resunance_animation.py
@@ -8,13 +8,13 @@ gray = (80,80,80)
 W = 360*2
 H = 360*2
 #מספר שורות
-N = 30
+N = 22
 # כמה פיקסלים אתה רוצה שיהיו יחידה אחת במסך
-scale = 12
+scale = 14
 # התדירות בה האות מתקבל
 FREQ = 20
 global MAX
-MAX = 0
+MAX = [0]
 ENERGY_LOSS = 0
 SCREEN = pygame.display.set_mode((W,H))
 PRESSURE = [[0 for i in range(N)] for j in range(N)]
@@ -37,21 +37,24 @@ def update():
 
 def puls(addition):
     for i in range(N):
-        PRESSURE[i][1]+=addition
+        PRESSURE[i][0]=addition
+    pygame.draw.line(SCREEN, white, (10, 70 + (N + 8) * scale),
+                     (W - 10, 70 + (N + 8) * scale), 1)
+
     if addition>0:
-        pygame.draw.line(SCREEN,((255 * addition) // 1, 20, 0),(10, 70 + (N +8-addition*4) * scale),(W-10, 70 + (N +8-addition*4) * scale),int(addition*5))
+        pygame.draw.line(SCREEN,((255 * addition) // 1, 50, 20),(10, 70 + (N +8-addition*4) * scale),(W-10, 70 + (N +8-addition*4) * scale),int(addition*5)+3)
     else:
 
-        pygame.draw.line(SCREEN,(0, 20, (255 * abs(addition)) // 1),(10, 70 + (N +8-addition*4) * scale),(W-10, 70 + (N +8-addition*4) * scale),int(abs(addition*5)))
+        pygame.draw.line(SCREEN,(20, 50, (255 * abs(addition)) // 1),(10, 70 + (N +8-addition*4) * scale),(W-10, 70 + (N +8-addition*4) * scale),int(abs(addition*5))+3)
 
 
 
 def draw():
 
-    m = max([max(k) for k in PRESSURE[2:]])
-    # if maxGrid>MAX:
-    #       MAX=maxGrid
-
+    maxGrid= max([max(k) for k in PRESSURE[2:]])
+    if maxGrid > MAX[0]:
+          MAX[0]=maxGrid
+    m = MAX[0]
     Emax = max (ENERGY)
     if Emax == 0:
         Emax = 1
@@ -64,7 +67,7 @@ def draw():
             if p>0:
                 if m!=0:
                     p = (p / m)
-                pygame.draw.circle(SCREEN, ((255 * p)//1, 40, 20), (i * 2*scale-30, 70+j * scale), scale // 3+scale*p//4)
+                pygame.draw.circle(SCREEN, ((255 * p)//1, 40, 20), (i * 2*scale-30, 70+j * scale), scale // 3+scale*p//3)
             else:
                 if m!=0:
                     p = (p / m)
@@ -72,7 +75,7 @@ def draw():
                     p=-p
                 if p>1:
                     p=1
-                pygame.draw.circle(SCREEN,(20,40,(255*p)//1),(i*2*scale-30,70+j*scale),scale//3+scale*p//4)
+                pygame.draw.circle(SCREEN,(20,40,(255*p)//1),(i*2*scale-30,70+j*scale),scale//3+scale*p//3)
 
 
     pygame.display.flip()
@@ -81,9 +84,6 @@ def draw():
 
 def reset():
     SCREEN.fill((0,0,0))
-    # pygame.draw.line(SCREEN,white,(25,35),(W+100,H+35))
-    # pygame.draw.line(SCREEN,white,(W//2,0),(W//2,H-1))
-    # pygame.draw.circle(SCREEN,(255,255,255,255),(W//2,H//2),scale,1)
 
 reset()
 
@@ -92,8 +92,15 @@ I = 0
 PATH = "C:\\Users\ohadz\Desktop\\frame\\"
 running =True
 while running:
-    # sleep(0.1)
-    add =math.sin(I*2*math.pi/FREQ)
+    # sleep(0.01)
+    if I<300:
+        add =math.sin(I*2*math.pi/FREQ)
+    if I==300:
+        PRESSURE = [[0 for i in range(N)] for j in range(N)]
+        VELOCITY = [[0 for i in range(N)] for j in range(N)]
+        MAX=[0]
+    if I >= 300:
+        add =0.5*( math.sin(I * 2 * math.pi / 32)+math.sin(I * 2 * math.pi / 42))
     reset()
     puls(add)
     update()
@@ -102,7 +109,7 @@ while running:
     draw()
     I+=1
 
-    pygame.image.save(SCREEN,PATH+str(I)+".png")
+    # pygame.image.save(SCREEN,PATH+str(I)+".png")
 
     k = pygame.key.get_pressed()
     # אם לוחצים q המשחק נגמר
